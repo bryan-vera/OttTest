00e4881d57541eadc4d2e5ba8ff8b66dd603bfbb
BVera Escritorio <BVera@DESKTOP-7K96HP5>
2021-07-22 12:15:00 -0500

    Creacion de un archivo txt de salida

--- OttTest/Program.cs
+++ OttTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Text;
 
 namespace OttTest
 {
@@ -27,20 +28,35 @@ namespace OttTest
 
         private static void lectura(string archivo)
         {
-            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), $@"Resources\{archivo}");
-            string[] lineas = File.ReadAllLines(path);
-            for (int i = 1; i < path.Length; i++)
+            string pathArchivoSalida = 
+                Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Resources\Salida.txt");
+            try
             {
-                var linea = lineas[i].Split(' ');
-                var reversa = revisarPalabra(linea);
-                Console.WriteLine($"Case #{i}: {reversa}");
+                using (FileStream fs = File.Create(pathArchivoSalida))
+                {
+                    string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), $@"Resources\{archivo}");
+                    string[] lineas = File.ReadAllLines(path);
+                    for (int i = 1; i < lineas.Length; i++)
+                    {
+                        var linea = lineas[i].Split(' ');
+                        var reversa = revisarPalabra(linea);
+                        var lineaSalida = $"Case #{i}: {reversa}";
+                        Console.WriteLine(lineaSalida);
+                        byte[] info = new UTF8Encoding(true).GetBytes(lineaSalida+"\n"); 
+                        fs.Write(info, 0, info.Length);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
             }
         }
 
         static void Main(string[] args)
         {
-            lectura("B-small-practice.in");
-            //lectura("B-large-practice.in");
+            //lectura("B-small-practice.in");
+            lectura("B-large-practice.in");
         }
     }
 }
